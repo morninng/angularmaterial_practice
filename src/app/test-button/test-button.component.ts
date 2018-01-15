3f30177cd1c52ed3e049f6c2692ad2470a775c9d
morninng <mr_morninng_star_dash@yahoo.co.jp>
2018-01-15 13:24:06 +0900

    test component added

---
+++ src/app/test-button/test-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-button',
+  templateUrl: './test-button.component.html',
+  styleUrls: ['./test-button.component.css']
+})
+export class TestButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
