8784f8fdec52179aba077db049befe31c9c99eb1
morninng <mr_morninng_star_dash@yahoo.co.jp>
2018-01-17 14:03:06 +0900

    snackbar component added

---
+++ src/app/snackbar-container/snackbar-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-snackbar-container',
+  templateUrl: './snackbar-container.component.html',
+  styleUrls: ['./snackbar-container.component.css']
+})
+export class SnackbarContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
